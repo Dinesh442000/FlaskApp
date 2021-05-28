bd6f95dba351b953552af666492fb08091779041
Dinesh442000 <54971831+Dinesh442000@users.noreply.github.com>
2021-05-28 20:40:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,64 +21,64 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/posts',methods = ['GET', 'POST'])
-def posts():
+# @app.route('/posts',methods = ['GET', 'POST'])
+# def posts():
 
-    if request.method == 'POST':
-        posts_title = request.form['title']
-        posts_author = request.form['author']
-        posts_content = request.form['content']
-        new_post = BlogPost(title = posts_title, content = posts_content,author = posts_author)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
-        return render_template('posts.html',posts = all_posts)
-
-
-# @app.route('/home/<string:name>')
-# def hello(name):
-#     return "Hello ,"+ name
-
-# @app.route('/onlyget', methods = ['GET'] )
-# def get_reg():
-#     return "You can only get this page"
+#     if request.method == 'POST':
+#         posts_title = request.form['title']
+#         posts_author = request.form['author']
+#         posts_content = request.form['content']
+#         new_post = BlogPost(title = posts_title, content = posts_content,author = posts_author)
+#         db.session.add(new_post)
+#         db.session.commit()
+#         return redirect('/posts')
+#     else:
+#         all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+#         return render_template('posts.html',posts = all_posts)
 
 
-@app.route('/posts/delete/<int:id>')
-def delete(id):
-    post = BlogPost.query.get_or_404(id)
-    db.session.delete(post)
-    db.session.commit()
-    return redirect('/posts')
+# # @app.route('/home/<string:name>')
+# # def hello(name):
+# #     return "Hello ,"+ name
+
+# # @app.route('/onlyget', methods = ['GET'] )
+# # def get_reg():
+# #     return "You can only get this page"
 
 
-@app.route('/posts/edit/<int:id>' , methods = ['GET','POST'])
-def edit(id):
-    post = BlogPost.query.get_or_404(id)
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.author = request.form['author']
-        post.content = request.form['content']
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        return render_template('edit.html',post=post)
+# @app.route('/posts/delete/<int:id>')
+# def delete(id):
+#     post = BlogPost.query.get_or_404(id)
+#     db.session.delete(post)
+#     db.session.commit()
+#     return redirect('/posts')
 
 
-@app.route('/posts/new' , methods = ['GET','POST'])
-def new_post():
-    if request.method == 'POST':
-        post_title = request.form['title']
-        post_author = request.form['author']
-        post_content = request.form['content']
-        new_post = BlogPost(title = post_title, content = post_content,author = post_author)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        return render_template('new_post.html')
+# @app.route('/posts/edit/<int:id>' , methods = ['GET','POST'])
+# def edit(id):
+#     post = BlogPost.query.get_or_404(id)
+#     if request.method == 'POST':
+#         post.title = request.form['title']
+#         post.author = request.form['author']
+#         post.content = request.form['content']
+#         db.session.commit()
+#         return redirect('/posts')
+#     else:
+#         return render_template('edit.html',post=post)
+
+
+# @app.route('/posts/new' , methods = ['GET','POST'])
+# def new_post():
+#     if request.method == 'POST':
+#         post_title = request.form['title']
+#         post_author = request.form['author']
+#         post_content = request.form['content']
+#         new_post = BlogPost(title = post_title, content = post_content,author = post_author)
+#         db.session.add(new_post)
+#         db.session.commit()
+#         return redirect('/posts')
+#     else:
+#         return render_template('new_post.html')
 
 
 if __name__ == "__main__":
